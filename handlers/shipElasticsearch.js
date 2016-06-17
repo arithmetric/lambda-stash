210e01958118c3cc7602947b643a29d6472c98d2
Joe Turgeon <arithmetric@gmail.com>
2016-06-17 17:14:37 -0500

    Refactoring the shipElasticsearch handler to allow setting a maximum chunk size and splitting bulk uploads into chunks.

--- handlers/shipElasticsearch.js
+++ handlers/shipElasticsearch.js
@@ -1,39 +1,63 @@
+var _ = require('lodash');
+
 exports.process = function(config) {
   console.log('shipElasticsearch');
-  return new Promise(function(resolve, reject) {
-    var esConfig = {
-      host: config.elasticsearch.host
+
+  var esConfig = {
+    host: config.elasticsearch.host
+  };
+
+  if (config.elasticsearch.useAWS) {
+    esConfig.connectionClass = require('http-aws-es');
+    esConfig.amazonES = {
+      region: config.elasticsearch.region
     };
-    if (config.elasticsearch.useAWS) {
-      esConfig.connectionClass = require('http-aws-es');
-      esConfig.amazonES = {
-        region: config.elasticsearch.region
-      };
-      if (config.elasticsearch.useEnvCreds) {
-        var AWS = require('aws-sdk');
-        esConfig.amazonES.credentials = new AWS.EnvironmentCredentials('AWS');
-      } else if (config.elasticsearch.accessKey &&
-          config.elasticsearch.secretKey) {
-        esConfig.amazonES.accessKey = config.elasticsearch.accessKey;
-        esConfig.amazonES.secretKey = config.elasticsearch.secretKey;
-      }
+    if (config.elasticsearch.useEnvCreds) {
+      var AWS = require('aws-sdk');
+      esConfig.amazonES.credentials = new AWS.EnvironmentCredentials('AWS');
+    } else if (config.elasticsearch.accessKey &&
+        config.elasticsearch.secretKey) {
+      esConfig.amazonES.accessKey = config.elasticsearch.accessKey;
+      esConfig.amazonES.secretKey = config.elasticsearch.secretKey;
     }
-    var es = require('elasticsearch').Client(esConfig); // eslint-disable-line new-cap
-    var num = config.data.length;
-    var i;
-    var docs = [];
-    for (i = 0; i < num; i++) {
-      docs.push({
-        index: {
-          _index: config.elasticsearch.index,
-          _type: config.elasticsearch.type
-        }
+  }
+
+  var es = config.drivers.ES || require('elasticsearch').Client(esConfig); // eslint-disable-line new-cap
+  var docs = [];
+  var maxChunkSize = config.elasticsearch.maxChunkSize || 1000;
+  var promises = [];
+
+  var ship = function(docs) {
+    console.log('Preparing to ship ' + (docs.length / 2) +
+      ' records to Elasticsearch.');
+    return new Promise(function(resolve, reject) {
+      es.bulk({body: docs}, function(err /* , result */) {
+        return (err) ? reject(err) : resolve();
       });
-      docs.push(config.data[i]);
-    }
-    console.log('Preparing to ship ' + num + ' records to Elasticsearch.');
-    es.bulk({body: docs}, function(err /* , result */) {
-      return (err) ? reject(err) : resolve(config);
     });
+  };
+
+  _.forEach(config.data, function(datum) {
+    docs.push({
+      index: {
+        _index: config.elasticsearch.index,
+        _type: config.elasticsearch.type
+      }
+    });
+    docs.push(datum);
+
+    if ((docs.length / 2) >= maxChunkSize) {
+      var chunk = docs;
+      promises.push(ship(chunk));
+      docs = [];
+    }
   });
+  if (docs.length) {
+    promises.push(ship(docs));
+  }
+
+  return Promise.all(promises)
+    .then(function() {
+      return config;
+    });
 };
