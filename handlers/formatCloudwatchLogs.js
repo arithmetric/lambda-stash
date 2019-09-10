3abd04067d0a7a410222bbc78259b10ac10ae468
Joe Turgeon <arithmetric@gmail.com>
2019-09-10 15:02:39 -0500

    Fixing parsing of Cloudwatch Logs streamed to Lambda.

--- handlers/formatCloudwatchLogs.js
+++ handlers/formatCloudwatchLogs.js
@@ -21,30 +21,33 @@ exports.process = function(config) {
     if (parts && parts[1] === 'START') {
       item.logType = 'start';
       parts = item.message.match(/^START RequestId: ([a-z0-9-]+) Version: (\S+)/); // eslint-disable-line max-len
-      if (parts && parts.length === 3) {
+      if (parts && parts.length >= 3) {
         item.requestId = parts[1];
         item.lambdaVersion = parts[2];
+        delete item.message;
       }
     } else if (parts && parts[1] === 'REPORT') {
       item.logType = 'report';
-      parts = item.message.match(/^REPORT RequestId: ([a-z0-9-]+)\tDuration: ([0-9.]+) ms\tBilled Duration: ([0-9.]+) ms \tMemory Size: ([0-9.]+) MB\tMax Memory Used: ([0-9.]+)/); // eslint-disable-line max-len
-      if (parts && parts.length === 6) {
+      parts = item.message.match(/^REPORT RequestId: ([a-z0-9-]+)\tDuration: ([0-9.]+) ms\tBilled Duration: ([0-9.]+) ms\tMemory Size: ([0-9.]+) MB\tMax Memory Used: ([0-9.]+)/); // eslint-disable-line max-len
+      if (parts && parts.length >= 6) {
         item.requestId = parts[1];
         item.duration = parts[2];
         item.durationBilled = parts[3];
         item.memConfigured = parts[4];
         item.memUsed = parts[5];
+        delete item.message;
       }
     } else if (parts && parts[1] === 'END') {
       item.logType = 'end';
       parts = item.message.match(/^END RequestId: ([a-z0-9-]+)/);
       if (parts && parts[1]) {
         item.requestId = parts[1];
+        delete item.message;
       }
     } else {
       item.logType = 'message';
       parts = item.message.match(/^(.*)\t(.*)\t((.|\n)*)/m);
-      if (parts && parts.length === 5) {
+      if (parts && parts.length >= 5) {
         item.requestId = parts[2];
         item.message = parts[3];
       }
